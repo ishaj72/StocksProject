d67d3c536415eed467689137e8bf8ec2ddb2ca3e
Isha Jain <75637232+ishaj72@users.noreply.github.com>
2026-02-17 11:17:36 +0530

    Update RabbitMQConfig.java

--- onboarding/src/main/java/com/example/onboarding/config/RabbitMQConfig.java
+++ onboarding/src/main/java/com/example/onboarding/config/RabbitMQConfig.java
@@ -17,16 +17,16 @@ public class RabbitMQConfig {
     // Routing key (from producer)
     public static final String ROUTING_KEY = "notification.otp.send";
     
-    // Your queue name (you can choose any name)
+    // My queue name
     public static final String QUEUE_NAME = "otp-notification-queue";
 
-    // Declare the Topic Exchange (must match producer's exchange)
+    // Declaring the Topic Exchange 
     @Bean
     public TopicExchange exchange() {
         return new TopicExchange(EXCHANGE_NAME);
     }
 
-    // Declare your Queue
+    //  My Queue
     @Bean
     public Queue queue() {
         return new Queue(QUEUE_NAME, true); // durable = true
