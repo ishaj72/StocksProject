6e9ff5c4fe85756083c15ed986e379b9a44dd9d5
Isha <isha30435@gmail.com>
2026-02-20 20:54:51 +0530

    creating constants

--- onboarding/src/main/java/com/example/constants/Constants.java
+++ onboarding/src/main/java/com/example/constants/Constants.java
@@ -1,9 +1,23 @@
 package com.example.constants;
 
 public class Constants {
-
+    
+    /*Named Query */
     public static final String GET_ONBOARD_INFO_BY_USER_ID = "getOnboardInfoByUserId";
+
+    /*Error message constants */
     public static final String SOMETHING_WENT_WRONG = "Something went wrong.";
 
-     private Constants() {}
+    /*Rabbit MQ Constants */
+
+    //Producer constants
+    public static final String PRODUCER_EXCHANGE_NAME = "user.topic";
+    public static final String PRODUCER_ROUTING_KEY = "user.info.send";
+
+   // Consumer constants
+    public static final String EXCHANGE_NAME = "auth.topic";
+    public static final String ROUTING_KEY = "user.registered";
+    public static final String QUEUE_NAME = "otp-notification-queue";
+
+    private Constants() {}
 }
--- onboarding/src/main/java/com/example/service/rabbitMQ/RabbitMQConsumer.java
+++ onboarding/src/main/java/com/example/service/rabbitMQ/RabbitMQConsumer.java
@@ -4,9 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-
 import com.example.modules.OnboardInfo;
-import com.example.modules.UserEvent;
 
 
 import lombok.extern.slf4j.Slf4j;
