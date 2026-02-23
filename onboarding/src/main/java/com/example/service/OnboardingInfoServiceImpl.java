7052d7bf69a3802ba210f34d8b94e0cbd05f5fbd
Isha <isha30435@gmail.com>
2026-02-23 23:40:55 +0530

    Adding save call to work with rabbitMQ and save/update data

--- onboarding/src/main/java/com/example/service/OnboardingInfoServiceImpl.java
+++ onboarding/src/main/java/com/example/service/OnboardingInfoServiceImpl.java
@@ -3,26 +3,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import com.example.constants.Constants;
 import com.example.dao.interfaces.IOnboardInfoDao;
+import com.example.modules.AddressInfo;
 import com.example.modules.OnboardInfo;
 import com.example.service.interfaces.IOnboardingInfoService;
-import com.example.service.rabbitMQ.RabbitMQConsumer;
-import com.example.service.rabbitMQ.RabbitMQProducer;
+
+import jakarta.persistence.EntityManager;
+
 
 
 @Service
+@Transactional
 public class OnboardingInfoServiceImpl implements IOnboardingInfoService {
 
     @Autowired
     private IOnboardInfoDao onboardInfoDao;
 
     @Autowired
-    private RabbitMQConsumer rabbitMQConsumer;
+    private EntityManager entityManager;
 
-    private static final Logger log = LoggerFactory.getLogger(RabbitMQProducer.class);
+    private static final Logger log = LoggerFactory.getLogger(OnboardingInfoServiceImpl.class);
 
     @Override
     public void saveOnboardingInfo(OnboardInfo onboardInfo) {
