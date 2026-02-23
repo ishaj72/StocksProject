5585f5cc7516638ac7dc473e28b63d16dcf3d696
Isha <isha30435@gmail.com>
2026-02-23 23:56:29 +0530

    config fix

--- onboarding/src/main/java/com/example/onboarding/config/SecurityConfig.java
+++ onboarding/src/main/java/com/example/onboarding/config/SecurityConfig.java
@@ -1,22 +1,42 @@
 package com.example.onboarding.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.security.JwtAuthFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-     @Bean
+
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+    
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()  // Allow all requests for testing
-            );
-        
+                .anyRequest().authenticated()  // all requests need valid JWT
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+    //  @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .csrf(csrf -> csrf.disable())
+    //         .authorizeHttpRequests(auth -> auth
+    //             .anyRequest().permitAll()  // Allow all requests for testing
+    //         );
+        
+    //     return http.build();
+    // }
 }
